632785586c84f12dc1df6cd1934784f95fedf5e2
ihsanulrahman <ihsanulrahman@gmail.com>
2025-02-12 01:55:57 +0500

    replace git link with github.io

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Copyright © 2022-<script>document.write( new Date().getFullYear() );</script> Powered by <a href="https://github.com/ihsanulrahman/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a> & Made by <a href=\"https://github.com/ihsanulrahman\" target=\"_blank\" rel=\"noopener noreferrer\">iHSAN</a>',
+    'Copyright © 2022-<script>document.write( new Date().getFullYear() );</script> Powered by <a href="https://github.com/ihsanulrahman/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a> & Made by <a href=\"https://ihsanulrahman.github.io\" target=\"_blank\" rel=\"noopener noreferrer\">iHSAN</a>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -57,7 +57,7 @@ module.exports = {
     },
     {
       name: 'Github',
-      link: 'https://github.com/ihsanulrahman',
+      link: 'https://ihsanulrahman.github.io',
     }
   ],
 
